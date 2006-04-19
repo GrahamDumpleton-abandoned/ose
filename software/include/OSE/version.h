b000f187e5375cb122040b2ba0c651a88d47ff36
grahamd <devnull@localhost>
2006-04-19 22:23:19 +0000

    Increment version to 8.1.

--- software/include/OSE/version.h
+++ software/include/OSE/version.h
@@ -10,13 +10,13 @@ extern int OSE_DEPENDENCY_MARKER;
 
 /* Library release information. */
 
-#define OSE_RELEASE_NAME "8.0b9"
-#define OSE_RELEASE_NUMBER "80000"
+#define OSE_RELEASE_NAME "8.1"
+#define OSE_RELEASE_NUMBER "80100"
 
-#define OSE_LIBRARY_RELEASE 80000
+#define OSE_LIBRARY_RELEASE 80100
 
 #define OSE_MAJOR_RELEASE 8
-#define OSE_MINOR_RELEASE 00
+#define OSE_MINOR_RELEASE 01
 #define OSE_MAINTENANCE_RELEASE 00
 
 /* ------------------------------------------------------------------------- */
