f80ce13f46c73577c1954c3064f74e00ef1bb888
grahamd <devnull@localhost>
2006-07-07 23:58:32 +0000

    Increment version to 8.2b1 and fix typo in changes file.

--- software/include/OSE/version.h
+++ software/include/OSE/version.h
@@ -10,13 +10,13 @@ extern int OSE_DEPENDENCY_MARKER;
 
 /* Library release information. */
 
-#define OSE_RELEASE_NAME "8.1"
-#define OSE_RELEASE_NUMBER "80100"
+#define OSE_RELEASE_NAME "8.2b1"
+#define OSE_RELEASE_NUMBER "80200"
 
-#define OSE_LIBRARY_RELEASE 80100
+#define OSE_LIBRARY_RELEASE 80200
 
 #define OSE_MAJOR_RELEASE 8
-#define OSE_MINOR_RELEASE 01
+#define OSE_MINOR_RELEASE 02
 #define OSE_MAINTENANCE_RELEASE 00
 
 /* ------------------------------------------------------------------------- */
