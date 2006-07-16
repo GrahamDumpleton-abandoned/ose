d6e4ca9b2fab045bfc052aaa5eaf02268581824b
grahamd <devnull@localhost>
2006-07-16 11:19:19 +0000

    Back ported from OSE 8.2 features which allow throttling of HTTP connections.

--- lib/OTC/internet/_httpdaem.cc
+++ lib/OTC/internet/_httpdaem.cc
@@ -11,13 +11,14 @@
 //     Graham Dumpleton
 // 
 // = COPYRIGHT
-//     Copyright 1999-2003 DUMPLETON SOFTWARE CONSULTING PTY LIMITED
+//     Copyright 1999-2006 DUMPLETON SOFTWARE CONSULTING PTY LIMITED
 //
 // ============================================================================
 */
 
 #include <OTC/internet/httpdaem.hh>
 
+#include <OTC/message/connect.hh>
 #include <OTC/dispatch/dispatch.hh>
 #include <OTC/dispatch/shutdown.hh>
 #include <OTC/dispatch/timeout.hh>
@@ -28,6 +29,85 @@
 #include <stdlib.h>
 
 /* ------------------------------------------------------------------------- */
+
+class TST_Throttle : public OTC_EVAgent
+{
+  public:
+
+                        TST_Throttle(
+                         OTC_HttpDaemon* theDaemon,
+                         u_int theSuspendThreshold,
+                         u_int theResumeThreshold
+                        );
+
+  protected:
+
+    void                handle(OTC_Event* theEvent);
+
+  private:
+
+    OTC_HttpDaemon*     daemon_;
+
+    u_int               suspendThreshold_;
+
+    u_int               resumeThreshold_;
+
+    bool                isSuspended_;
+};
+
+TST_Throttle::TST_Throttle(
+ OTC_HttpDaemon* theDaemon,
+ u_int theSuspendThreshold,
+ u_int theResumeThreshold
+)
+  : daemon_(theDaemon), suspendThreshold_(theSuspendThreshold),
+    resumeThreshold_(theResumeThreshold), isSuspended_(false)
+{
+  daemon_->addObserver(id());
+}
+
+void TST_Throttle::handle(OTC_Event* theEvent)
+{
+  if (theEvent == 0)
+    return;
+
+  cerr << daemon_->numClients() << endl;
+
+  if (theEvent->type() == OTCEV_Connection::typeId())
+  {
+    OTCEV_Connection* theConnection;
+    theConnection = (OTCEV_Connection*)theEvent;
+
+    if (theConnection->state() == OTCLIB_CONNECTION_ACTIVE)
+    {
+      if (daemon_->numClients() >= suspendThreshold_)
+      {
+        if (!isSuspended_)
+        {
+          daemon_->suspendListening();
+          isSuspended_ = true;
+
+          cerr << "suspend" << endl;
+        }
+      }
+    }
+    else
+    {
+      if (isSuspended_)
+      {
+        if (daemon_->numClients() < resumeThreshold_)
+        {
+          daemon_->resumeListening();
+          isSuspended_ = false;
+
+          cerr << "resume" << endl;
+        }
+      }
+    }
+  }
+
+  theEvent->destroy();
+}
 
 void test1()
 {
@@ -42,6 +122,8 @@ void test1()
   theDaemon.attach("/1",&theServer);
   theDaemon.attach("/1/2",&theServer);
   theDaemon.attach("/1/2/3",&theServer);
+
+  TST_Throttle theThrottle(&theDaemon,5,2);
 
   theDaemon.start();
 
